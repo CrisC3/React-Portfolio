dcc61970330e2290bc1f892c6a315336190b60d0
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-20 10:52:42 -0500

    Added basic sectionMain

--- src/components/sectionHeader.js
+++ src/components/sectionHeader.js
@@ -1,19 +1,23 @@
 import React from "react";
+import SectionMain from "./sectionMain";
 
 function SectionHeader() {
 
     return (
-        <header>
-            <div class="header-main d-flex flex-column flex-md-row text-center">
-                <h1>Cristobal Canales</h1>
-            </div>
-            <div class="header-img-msg mt-2">
-                <img id="personal-img" src="images/personal.png" alt="Profile picture" />
-                <div id="greeting-msg">
-                    <h1 >Welcome to My Page!</h1>
+        <div>
+            <header>
+                <div class="header-main d-flex flex-column flex-md-row text-center">
+                    <h1>Cristobal Canales</h1>
                 </div>
-            </div>
-        </header>
+                <div class="header-img-msg mt-2">
+                    <img id="personal-img" src="images/personal.png" alt="Profile picture" />
+                    <div id="greeting-msg">
+                        <h1>Welcome to My Page!</h1>
+                    </div>
+                </div>
+            </header>
+            <SectionMain />
+        </div>
     );
 }
 
---
+++ src/components/sectionMain.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SectionMain() {
+
+    return (
+        <h1>Section Main</h1>
+    );
+}
+
+export default SectionMain;
