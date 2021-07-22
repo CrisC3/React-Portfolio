e096169f435e843be0096dcab5da27cca0320812
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-21 22:53:48 -0500

    Modified styles properties

--- src/components/sectionNavbar.js
+++ src/components/sectionNavbar.js
@@ -3,32 +3,33 @@ import React from "react";
 function SectionNavbar({ currentPage, handlePageChange }) {
 
     const styles = {
-        color: "black",
-        textDecoration: "none",
-        margin: "auto 0.2rem",
-        fontWeight: "bold",
-        borderTopStyle: "solid",
-        borderRightStyle: "solid",
-        borderLeftStyle: "solid",
-        borderColor: "white",
-        padding: "0.15rem",
-        backgroundColor: "white"
+        navUl: {
+            backgroundColor: "lightgoldenrodyellow"
+        },
+        links: {
+            // borderTopStyle: "solid",
+            // borderRightStyle: "solid",
+            // borderLeftStyle: "solid",
+            borderStyle: "solid",
+            borderColor: "black",
+            borderRadius: "0px",
+            // backgroundColor: "white"
+            fontWeight: "bold"
+        }
       };
-
-    return (
-        <div class="container">
-            <ul className="nav nav-tabs align-self-md-end">
-                <li className="nav-item">
-                    <a href="#home" onClick={() => handlePageChange("Home")} style={styles} className={currentPage === "Home" ? "nav-link active" : "nav-link"}>Home</a>
+    
+      return (
+          <ul className="nav nav-tabs align-self-md-end" style={styles.navUl}>
+              <li className="nav-item">
+                <a href="#home" onClick={() => handlePageChange("Home")} style={styles.links} className={currentPage === "Home" ? "nav-link active" : "nav-link"}>Home</a>
                 </li>
-                <li className="nav-item">
-                    <a href="#work" onClick={() => handlePageChange("Work")} style={styles} className={currentPage === "Home" ? "nav-link active" : "nav-link"}>Work</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#contact-me" onClick={() => handlePageChange("ContactMe")} style={styles} className={currentPage === "Home" ? "nav-link active" : "nav-link"}>Contact Me</a>
-                </li>
-            </ul>
-        </div>
+            <li className="nav-item">
+                <a href="#work" onClick={() => handlePageChange("Work")} style={styles.links} className={currentPage === "Work" ? "nav-link active" : "nav-link"}>Work</a>
+            </li>
+            <li className="nav-item">
+                <a href="#contact-me" onClick={() => handlePageChange("ContactMe")} style={styles.links} className={currentPage === "ContactMe" ? "nav-link active" : "nav-link"}>Contact Me</a>
+            </li>
+        </ul>
     );
 }
 
