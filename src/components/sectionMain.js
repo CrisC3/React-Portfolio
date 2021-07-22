7f0a7f119028563ead0e5bf64c2cdc37511cfdb0
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-21 21:15:26 -0500

    Added renderPage and handlePageChange variable functions

--- src/components/sectionMain.js
+++ src/components/sectionMain.js
@@ -3,6 +3,20 @@ import React, { useState } from "react";
 function SectionMain() {
     
     const [currentPage, setCurrentPage] = useState("Home");
+    
+    const renderPage = () => {
+        if (currentPage === "Home") {
+        //   return <Home />;
+        }
+        if (currentPage === "Work") {
+        //   return <Work />;
+        }
+        if (currentPage === "ContactMe") {
+        //   return <ContactMe />;
+        }
+      };
+
+    const handlePageChange = (page) => setCurrentPage(page);
 
     return (
         <h1>Section Main</h1>
